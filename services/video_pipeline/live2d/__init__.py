ae967a975d26e5360ec42f9725992c0e2478ba9d
minf417-ops <minf417@163.com>
2026-04-13 18:55:47 +0800

    清理无关 Live2D 文件

--- services/video_pipeline/live2d/__init__.py
+++
@@ -1,5 +0,0 @@
-from .controller import Live2DController
-from .mood_motion_map import MoodMotionMapper
-from .lipsync import LipSyncAnalyzer
-
-__all__ = ["Live2DController", "MoodMotionMapper", "LipSyncAnalyzer"]
